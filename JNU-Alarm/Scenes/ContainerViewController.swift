d7692265d4636b9448336b5e8141ac04199fcda0
rayleigh <rayleigh190@gmail.com>
2024-03-17 15:06:34 +0900

    feat: 배너광고 백그라운드에서 복귀 시 로드 적용 및 기본배너 적용

--- JNU-Alarm/Scenes/ContainerViewController.swift
+++ JNU-Alarm/Scenes/ContainerViewController.swift
@@ -23,43 +23,70 @@ class ContainerViewController: UIViewController {
         return view
     }()
     
-    var bannerView: GADBannerView!
+    private lazy var bannerView: GADBannerView = {
+        bannerView = GADBannerView(adSize: adSizeType)
+        bannerView.alpha = 0
+        bannerView.delegate = self
+        return bannerView
+    }()
+    
+    private lazy var backgroundBannerView: UIView = {
+        let view = UIView()
+        
+        let image = UIImageView(image: UIImage(named: "banner_1"))
+        image.contentMode = .scaleAspectFit
+        
+        view.addSubview(image)
+        image.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            image.topAnchor.constraint(equalTo: view.topAnchor),
+            image.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            image.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            image.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        
-        bannerView = GADBannerView(adSize: adSizeType)
-        bannerView.alpha = 0
-        bannerView?.delegate = self
     
-        initAdMob()
         setupSubViews()
+        initAdMob()
         
         let childVC = MainTapBarController()
         childVC.view.frame = containerView.bounds
         addChild(childVC)
         containerView.addSubview(childVC.view)
         childVC.didMove(toParent: self)
+        
+        // 백그라운드 > 포그라운드 시 광고 로드
+        NotificationCenter.default.addObserver(self, selector: #selector(loadAd), name: UIApplication.willEnterForegroundNotification, object: nil)
     }
 
 }
 
 extension ContainerViewController {
     func initAdMob() {
-        guard let bannerView = bannerView else { return }
-        bannerView.adUnitID = "ca-app-pub-4183402691727093/5410662598"
+        var adUnitID: String
+        #if DEBUG
+            adUnitID = "ca-app-pub-3940256099942544/2934735716" // test id
+        #else
+            adUnitID = "ca-app-pub-4183402691727093/5410662598" // service id
+        #endif
+        bannerView.adUnitID = adUnitID
         bannerView.rootViewController = self
         loadAd()
     }
     
-    func loadAd() {
-        guard let bannerView = bannerView else { return }
+    @objc func loadAd() {
         bannerView.load(GADRequest())
     }
     
     func setupSubViews() {
         view.addSubview(containerView)
+        view.addSubview(backgroundBannerView)
         view.addSubview(bannerView)
         
         containerView.translatesAutoresizingMaskIntoConstraints = false
@@ -67,15 +94,23 @@ extension ContainerViewController {
             containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             containerView.topAnchor.constraint(equalTo: view.topAnchor),
-            containerView.bottomAnchor.constraint(equalTo: bannerView.topAnchor),
+            containerView.bottomAnchor.constraint(equalTo: backgroundBannerView.topAnchor),
+        ])
+        
+        backgroundBannerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            backgroundBannerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundBannerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundBannerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            backgroundBannerView.heightAnchor.constraint(equalToConstant: CGFloat(adHeight)),
         ])
         
         bannerView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            bannerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            bannerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            bannerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            bannerView.heightAnchor.constraint(equalToConstant: CGFloat(adHeight)),
+            bannerView.leadingAnchor.constraint(equalTo: backgroundBannerView.leadingAnchor),
+            bannerView.trailingAnchor.constraint(equalTo: backgroundBannerView.trailingAnchor),
+            bannerView.bottomAnchor.constraint(equalTo: backgroundBannerView.bottomAnchor),
+            bannerView.topAnchor.constraint(equalTo: backgroundBannerView.topAnchor),
         ])
     }
       
